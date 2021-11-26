2db6b28ba47ae85c6efd087de816e967d070565d
unknown <sdedek15@gmail.com>
2021-11-27 00:57:44 +0700

    upload

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: 'mongodb://owin:XyIgQHNrbVd3Jitp@cluster0-shard-00-00.ympk1.mongodb.net:27017,cluster0-shard-00-01.ympk1.mongodb.net:27017,cluster0-shard-00-02.ympk1.mongodb.net:27017/owinapi?ssl=true&replicaSet=atlas-1e08as-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
